e4659263c9db0ab5bfbcb36e0bd3f569096d6e6f
awaissaddiqui <awaissaddiqui@users.noreply.github.com>
2022-12-02 19:58:10 +0000

    deploy: 30a98b9aff7a53aa69c4092a9a3471354191a03e

---
+++ precache-manifest.cc8a950963ae06635c02aba7efac05a8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d90af5f2b72c30f32e27",
+    "url": "/Shopify-Frontend/static/css/main.1fbf8d5e.chunk.css"
+  },
+  {
+    "revision": "d90af5f2b72c30f32e27",
+    "url": "/Shopify-Frontend/static/js/main.d90af5f2.chunk.js"
+  },
+  {
+    "revision": "3f944d5300964c9a98c7",
+    "url": "/Shopify-Frontend/static/js/1.3f944d53.chunk.js"
+  },
+  {
+    "revision": "d3c2c4e0f42107b38f16",
+    "url": "/Shopify-Frontend/static/css/2.29bdd09c.chunk.css"
+  },
+  {
+    "revision": "d3c2c4e0f42107b38f16",
+    "url": "/Shopify-Frontend/static/js/2.d3c2c4e0.chunk.js"
+  },
+  {
+    "revision": "41bc38ae6229afcabb8d",
+    "url": "/Shopify-Frontend/static/js/runtime~main.41bc38ae.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Shopify-Frontend/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Shopify-Frontend/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Shopify-Frontend/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Shopify-Frontend/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "86038ab4865b4877a85be6c579e3ea08",
+    "url": "/Shopify-Frontend/static/media/man.86038ab4.png"
+  },
+  {
+    "revision": "fa306e40929caada920ca186c1241ac9",
+    "url": "/Shopify-Frontend/static/media/home3.fa306e40.jpeg"
+  },
+  {
+    "revision": "3f9c48bbc86eb199a378a621e2a931f8",
+    "url": "/Shopify-Frontend/static/media/mob.3f9c48bb.png"
+  },
+  {
+    "revision": "68e1a972e91671fb3b4708f6af004019",
+    "url": "/Shopify-Frontend/static/media/ecommerce marketing.68e1a972.webp"
+  },
+  {
+    "revision": "879febee605d571d9bc22a2dbb84ed35",
+    "url": "/Shopify-Frontend/static/media/logo.879febee.png"
+  },
+  {
+    "revision": "dd9663dbfbe620ff4cfacc92200d8a58",
+    "url": "/Shopify-Frontend/index.html"
+  }
+];
